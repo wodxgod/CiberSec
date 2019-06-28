81d93392d94c5ca2f6282cfdb6301c122ed41459
WodX <38795504+WodxTV@users.noreply.github.com>
2019-06-28 19:52:57 +0200

    Update cibersec.py

--- cibersec.py
+++ cibersec.py
@@ -1,7 +1,7 @@
 #Developed by wodx @ 26-06-2019
 import discord, subprocess, json, os, textwrap
 
-#These commands are blacklisted, as they're pretty useless in this project. More commands can be added
+#These commands are blacklisted, as they're pretty useless in this project.
 BLACKLISTED_COMMANDS = [
     'nano', 
     'leafpad',
@@ -16,6 +16,9 @@ OPERATORS = [
     '||'
 ]
 
+global last_process
+last_process = None
+
 CONFIG = json.loads(open('config/config.json').read())
 
 def error(message):
@@ -26,6 +29,15 @@ def warning(message):
 
 def success(message):
     return discord.Embed(title='**SUCCESS**', description=message, colour=discord.Colour.green())
+
+def print_banner():
+    print ('''
+            ╔═╗╦╔╗ ╔═╗╦═╗  ╔═╗╔═╗╔═╗       { Github: WodXTV }
+            ║  ║╠╩╗║╣ ╠╦╝  ╚═╗║╣ ║         { Discord: wodx#0666 }
+            ╚═╝╩╚═╝╚═╝╩╚═  ╚═╝╚═╝╚═╝ v1.4  { Twitter: @WodXOfficial }
+               Cyber Security Bot
+      Submission for Discord Hack Week 2019
+        ''')
 
 def main():
     #Bot token check
@@ -46,90 +58,111 @@ def main():
     #Ready event
     @client.event
     async def on_ready():
+        #Printing banner of CiberSec
+        print_banner()
+        
         print('[+] CiberSec is ready to use.')
-        print('CiberSec - Cyber security bot')
-        print('By: wodx')
-        print('Version: 1.1')
-        print('Made for Discord Hack Week.')
+
+        await client.change_presence(activity=discord.Game('Discord Hack Week 2019'))
         await client.user.edit(username='CiberSec')
+        with open('./resources/CyberSec-logo.png', 'rb') as file:
+            await client.user.edit(avatar=file.read())
 
     #Message event
     @client.event
     async def on_message(event_message):
+        global last_process
+
+        #User check
         if event_message.author == client.user:
             return
-        
+
+        #Role check
+        has_role = None
+        for role in event_message.author.roles:
+            if str(role).lower() == 'cibersec user':
+                has_role = True
+                break
+        if not has_role:
+            return
+
+        #Channel check
         event_channel = event_message.channel
+        if event_channel.id != int(CONFIG['config']['terminal']):
+            return
+
         try:
-            try:
-                if event_channel.id == int(CONFIG['config']['terminal']):
-                    command = str(event_message.content)
+            command = str(event_message.content)
 
-                    #Command analyzing
-                    for operator in OPERATORS:
-                        if ' %s ' % (operator) in command:
-                            command_split = command.split(' %s ' % (operator))
-                            for cmd in command_split:
-                                for cmd_blacklisted in BLACKLISTED_COMMANDS:
-                                    if cmd.startswith(cmd_blacklisted):
-                                        await event_channel.send(embed=error('The executed command contains a blacklisted command.'))
-                                        return
-                    for cmd_blacklisted in BLACKLISTED_COMMANDS:
-                        if command.lower().startswith(cmd_blacklisted):
-                            await event_channel.send(embed=error('The executed command is blacklisted.'))
-                            return
-
-                    #Clear command
-                    if 'clear' in command.lower():
-                        if command.lower() == 'clear history':
-                            await client.get_channel(int(CONFIG['config']['history'])).purge(limit=1000)
-                            await event_channel.send(embed=success('Command history has been cleared.'))
-                        else:
-                            await event_channel.purge(limit=1000)
-                            await event_channel.send(embed=success('Terminal has been cleared.'))
-                        return
-
-                    #Change directory command
-                    if 'cd ' in command:
-                        command_split = command.split(' ')
-                        for i, cmd in enumerate(command_split):
-                            if cmd == ' ':
-                                continue
-                            if cmd.lower() == 'cd':
-                                os.chdir(command_split[i + 1])
-                                output = 'Changed directory to %s' % (os.getcwd())
-                                await event_channel.send('```\n' + output + '\n```')
+            #Command analyzing
+            for operator in OPERATORS:
+                if ' %s ' % (operator) in command:
+                    command_split = command.split(' %s ' % (operator))
+                    for cmd in command_split:
+                        for cmd_blacklisted in BLACKLISTED_COMMANDS:
+                            if cmd.startswith(cmd_blacklisted):
+                                await event_channel.send(embed=error('The executed command contains a blacklisted command.'))
                                 return
-                        
-                    #Executing the shell command
-                    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+            for cmd_blacklisted in BLACKLISTED_COMMANDS:
+                if command.lower().startswith(cmd_blacklisted):
+                    await event_channel.send(embed=error('The executed command is blacklisted.'))
+                    return
 
-                    #Command output
-                    output = process.stdout.read() + process.stderr.read()
-                    output = output.decode()
+            #Clear command
+            if 'clear' in command.lower():
+                if command.lower() == 'clear history':
+                    await client.get_channel(int(CONFIG['config']['history'])).purge(limit=1000)
+                    await event_channel.send(embed=success('Command history has been cleared.'))
+                else:
+                    await event_channel.purge(limit=1000)
+                    await event_channel.send(embed=success('Terminal has been cleared.'))
+                return
 
-                    #Adding command to history
-                    try:
-                        await client.get_channel(int(CONFIG['config']['history'])).send('``\n' + command + '\n``')
-                    except:
-                        await event_channel.send(embed=error('Command history channel not found.'))
+            #Change directory command
+            if 'cd ' in command:
+                command_split = command.split(' ')
+                for i, cmd in enumerate(command_split):
+                    if cmd == ' ':
+                        continue
+                    if cmd.lower() == 'cd':
+                        os.chdir(command_split[i + 1])
+                        output = 'Changed directory to %s' % (os.getcwd())
+                        await event_channel.send('```\n' + output + '\n```')
                         return
+                        
+            #Executing the shell command
+            process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+                    
+            #Cancel command
+            if command.lower() == 'cancel' and last_process:
+                last_process.kill()
+                await event_channel.send(embed=success('Process canceled.'))
 
-                    #Handling +2000 chars output
-                    if len(output) >= 2000:
-                        await event_channel.send(embed=warning('Output is more than 2000 characters long. Please wait while all data is getting transferred.'))
-                        for output_piece in [output[i * 1000 : i * 1000 + 1000] for i, _ in enumerate(output[::1000])]:
-                            await event_channel.send('```\n' + output_piece + '\n```')
-                        await event_channel.send(embed=success('All data has been transferred.'))
+            #Command output
+            output = process.stdout.read() + process.stderr.read()
+            output = output.decode()
 
-                    else:
-                        if output.strip():
-                            await event_channel.send('```\n' + output + '\n```')
-                        else:
-                            await event_channel.send(embed=error('No output recieved.'))
+            #Adding command to history
+            try:
+                await client.get_channel(int(CONFIG['config']['history'])).send('``\n' + command + '\n``')
+            except:
+                await event_channel.send(embed=error('Command history channel not found.'))
+                return
 
-            except discord.DiscordException:
-                await event_channel.send(embed=error('Terminal channel not found.'))
+            #Handling +2000 chars output
+            if len(output) >= 2000:
+                async with event_channel.typing():
+                    await event_channel.send(embed=warning('Output is more than 2000 characters long. Please wait while all data is getting transferred.'))
+                    for output_piece in [output[i * 1000 : i * 1000 + 1000] for i, _ in enumerate(output[::1000])]:
+                        await event_channel.send('```\n' + output_piece + '\n```')
+                    await event_channel.send(embed=success('All data has been transferred.'))
+
+            else:
+                if output.strip():
+                    await event_channel.send('```\n' + output + '\n```')
+                else:
+                    await event_channel.send(embed=error('No output recieved.'))
+            last_process = process
 
         except Exception as e:
             await event_channel.send(embed=error('An error occurred: %s' % (e)))
